b58701fbeb9839453ee79d9851382135d6d99402
Rafal Stalka <rafal@saveatrain.com>
2023-12-08 18:53:41 +0100

    Update navigation and access methods in web pages
    Added more access methods to ContactUsPage and AboutUsPage classes, to handle navigation actions efficiently. The changes involve extracting navigation actions into separate methods to avoid redundancy and enhance readability. A new constant ABOUT_US_PAGE is also introduced for better site mapping.

--- src/main/java/pages/AboutUsPage.java
+++ src/main/java/pages/AboutUsPage.java
@@ -4,19 +4,39 @@ import com.microsoft.playwright.Page;
 import constans.AppConstants;
 
 public class AboutUsPage {
-
     public Page page;
 
     public AboutUsPage(Page page) {
         this.page = page;
     }
 
-    public String getAboutUsPageTitle() {
+    private void navigateToAboutUs() {
         page.navigate(AppConstants.SAT_HOME_PAGE);
         // User clicks on about us tab
         page.click("id=about-us");
-        String title =  page.title();
+    }
+
+    public String getAboutUsPageTitle() {
+        navigateToAboutUs();
+        String title = page.title();
         System.out.println("page title: " + title);
         return title;
+    }
+
+    public String getAboutUsPageURL() {
+        navigateToAboutUs();
+        String url = page.url();
+        System.out.println("page url : " + url);
+        return url;
+    }
+
+    public String readMoreButtonFunctionality() {
+        page.navigate(AppConstants.SAT_HOME_PAGE);
+        // Click on Read More button
+        page.click(".services-body-desktop > .more-button");
+        // Checking title
+        String readMoreRedirectedPageTitle = page.title();
+        System.out.println("page title: " + readMoreRedirectedPageTitle);
+        return readMoreRedirectedPageTitle;
     }
 }
