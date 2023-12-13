d672bc8192e572c798a206a6e63c7cf8d1bc1b14
Rafal Stalka <rafal@saveatrain.com>
2023-12-13 17:30:44 +0100

    Refactor API product pages for improved navigation and information retrieve
    In this commit, we've significantly improved the existing structure for the API product pages including RailSearchApiProductPage, RailBookApiProductPage, RailForwardApiPage, and RailAgentDashboardProductPage. By introducing private navigation methods and explicit locators, we've enhanced code readability and maintainability. We've also added new methods to retrieve page URLs, expanding the available information for each page.

--- src/main/java/pages/RailBookApiProductPage.java
+++ src/main/java/pages/RailBookApiProductPage.java
@@ -3,23 +3,39 @@ package pages;
 import com.microsoft.playwright.Page;
 import constans.AppConstants;
 
+/**
+ * The RailBookApiProductPage class represents a page that contains rail book API information.
+ */
 public class RailBookApiProductPage {
-
     public Page page;
+    private static final String PRODUCTS_TAB_ID = "id=products";
+    private static final String RAIL_BOOK_API_ID = "id=rails-book-api";
 
     public RailBookApiProductPage(Page page) {
         this.page = page;
     }
 
-    public String getRailBookApiPageTitle() {
+    private Page navigateToRailBookApiPage() {
         page.navigate(AppConstants.SAT_HOME_PAGE);
         // User clicks on products tab
-        page.click("id=products");
+        page.click(PRODUCTS_TAB_ID);
         // User clicks on rail book api
-        page.click("id=rails-book-api");
-        String title =  page.title();
+        page.click(RAIL_BOOK_API_ID);
+
+        return page;
+    }
+
+    public String getRailBookApiPageTitle() {
+        Page page = navigateToRailBookApiPage();
+        String title = page.title();
         System.out.println("page title: " + title);
         return title;
     }
 
+    public String getRailSearchApiPageURL() {
+        Page page = navigateToRailBookApiPage();
+        String url = page.url();
+        System.out.println("page url : " + url);
+        return url;
+    }
 }
